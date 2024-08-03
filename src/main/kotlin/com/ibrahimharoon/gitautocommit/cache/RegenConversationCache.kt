f53e5689aa1270649ac0c60434771a6439556cb8
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2024-08-03 19:30:27 -0400

    - Update RegenConversationCache to include clear() method and counter increment. - Modify TerminalGui to display conversationCache during Regen. - Added clear() method to RegenConversationCache to reset cache when needed.

--- src/main/kotlin/com/ibrahimharoon/gitautocommit/cache/RegenConversationCache.kt
+++ src/main/kotlin/com/ibrahimharoon/gitautocommit/cache/RegenConversationCache.kt
@@ -6,10 +6,15 @@ object RegenConversationCache: LocalCache<String, String> {
 
     override fun set(key: String, value: String) {
         cache["$key-$counter"] = value
+        counter++
     }
 
     fun isNotEmpty(): Boolean {
         return cache.isNotEmpty()
+    }
+
+    fun clear() {
+        cache.clear()
     }
 
     override fun toString(): String {
