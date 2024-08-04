ea6e0dadd059f4651ee828a3bcb3e9130630fd43
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2024-08-04 16:04:14 -0400

    **Detailed Message:**
    **Summary:**
    Refactored GithubService to GithubReleaseService for clarity and consistency.
    
    **Changes:**
    - Renamed `GithubService` to `GithubReleaseService` for more descriptive naming.
    - Updated imports and usages of `GithubService` to `GithubReleaseService`.
    - Updated function names and references to align with the new service name.
    - Updated API URL endpoint and constants in `GithubReleaseService`.
    - Moved `GithubReleaseDto` import to correct location in `GithubReleaseService`.

--- src/main/kotlin/com/ibrahimharoon/gitautocommit/services/GithubReleaseService.kt
+++ src/main/kotlin/com/ibrahimharoon/gitautocommit/services/GithubReleaseService.kt
@@ -1,10 +1,10 @@
 package com.ibrahimharoon.gitautocommit.services
 
+import com.ibrahimharoon.gitautocommit.rest.dtos.GithubReleaseDto
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.client.getForObject
-import com.ibrahimharoon.gitautocommit.rest.dtos.GithubReleaseDto
 
-object GithubService {
+object GithubReleaseService {
     private const val GITHUB_BASE_URL = "https://api.github.com"
     private const val GITHUB_REPO = "repos/Ibrahim-Haroon/git-autocommit-cli"
     private const val GITHUB_API_URL = "$GITHUB_BASE_URL/$GITHUB_REPO/releases/latest"
