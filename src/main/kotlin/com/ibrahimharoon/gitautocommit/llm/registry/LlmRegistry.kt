422471211d7b93fb0303d148b58df567350623c1
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2024-08-04 13:30:48 -0400

    **Detailed Message:**
    **Summary:**
    Refactor LlmRegistry classes and move LlmTemplates to correct package
    
    **Changes:**
    - Deleted LlmRegistry and LlmRegistryStore classes
    - Moved LlmTemplates into correct package in Llm directory

--- src/main/kotlin/com/ibrahimharoon/gitautocommit/llm/registry/LlmRegistry.kt
+++
@@ -1,7 +0,0 @@
-package com.ibrahimharoon.gitautocommit.llm.registry
-
-import com.ibrahimharoon.gitautocommit.llm.LlmType
-
-@Target(AnnotationTarget.CLASS)
-@Retention(AnnotationRetention.RUNTIME)
-annotation class LlmRegistry(val llm: LlmType)
