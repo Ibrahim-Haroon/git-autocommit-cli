b21d0784c3a13b615450430e69d8c77a84f7c2cf
Ibrahim Haroon <99413953+Ibrahim-Haroon@users.noreply.github.com>
2024-12-20 02:22:44 -0500

    Revert "added functionality to use conversation memory object for storing previous conversations. Made thread safe as well"
    This reverts commit f9fcaab758a78551494e742a785743d9c3cc2142.

--- src/main/kotlin/com/ibrahimharoon/gitautocommit/llm/service/DefaultLlmResponseService.kt
+++ src/main/kotlin/com/ibrahimharoon/gitautocommit/llm/service/DefaultLlmResponseService.kt
@@ -1,38 +1,25 @@
 package com.ibrahimharoon.gitautocommit.llm.service
 
-import com.ibrahimharoon.gitautocommit.llm.model.LlmMessage
 import com.ibrahimharoon.gitautocommit.rest.dtos.DefaultLlmResponseDto
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpMethod
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.client.exchange
-import java.util.concurrent.locks.ReentrantLock
-import kotlin.concurrent.withLock
 
 class DefaultLlmResponseService(
     private val model: String,
     private val url: String,
     private val headers: HttpHeaders
 ) : LlmResponseService {
-    private val lock = ReentrantLock()
-
-    override fun response(role: String, prompt: String, conversationHistory: List<LlmMessage>?): String = lock.withLock {
-        val prevConversations: List<Map<String, String>> = conversationHistory?.map { message ->
-            mapOf(
-                "role" to message.role.value,
-                "content" to message.content
-            )
-        } ?: emptyList()
-
+    override fun response(role: String, prompt: String): String {
         val payload = mapOf(
             "model" to model,
             "messages" to listOf(
                 mapOf(
                     "role" to "system",
                     "content" to role
-                )
-            ) + prevConversations + listOf(
+                ),
                 mapOf(
                     "role" to "user",
                     "content" to prompt
--- src/main/kotlin/com/ibrahimharoon/gitautocommit/llm/service/GoogleVertexLlmResponseService.kt
+++ src/main/kotlin/com/ibrahimharoon/gitautocommit/llm/service/GoogleVertexLlmResponseService.kt
@@ -1,38 +1,20 @@
 package com.ibrahimharoon.gitautocommit.llm.service
 
-import com.ibrahimharoon.gitautocommit.llm.model.LlmMessage
 import com.ibrahimharoon.gitautocommit.rest.dtos.GoogleVertexResponseDto
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpMethod
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.client.exchange
-import java.util.concurrent.locks.ReentrantLock
-import kotlin.concurrent.withLock
 
 class GoogleVertexLlmResponseService(
     private val url: String,
     private val headers: HttpHeaders
 ) : LlmResponseService {
-    private val lock = ReentrantLock()
 
-    override fun response(role: String, prompt: String, conversationHistory: List<LlmMessage>?): String = lock.withLock {
-        val prevConversations = conversationHistory?.map { message ->
-            mapOf(
-                "role" to when (message.role.value) {
-                    "assistant" -> "model"
-                    else -> "user"
-                },
-                "parts" to listOf(
-                    mapOf(
-                        "text" to message.content
-                    )
-                )
-            )
-        } ?: emptyList()
-
+    override fun response(role: String, prompt: String): String {
         val payload = mapOf(
-            "contents" to prevConversations + listOf(
+            "contents" to listOf(
                 mapOf(
                     "role" to "user",
                     "parts" to listOf(
