6d8ab5163ec1735f06133e2b0506e9c4a101b202
Ibrahim Haroon <99413953+Ibrahim-Haroon@users.noreply.github.com>
2024-12-20 02:22:44 -0500

    Revert "feat: Added LlmMessage model and updated LlmResponseService interface method signature"
    This reverts commit 2bcfa6aabea62770940ae4531dc96cc8e5ba516b.

--- src/main/kotlin/com/ibrahimharoon/gitautocommit/llm/service/LlmResponseService.kt
+++ src/main/kotlin/com/ibrahimharoon/gitautocommit/llm/service/LlmResponseService.kt
@@ -1,15 +1,12 @@
 package com.ibrahimharoon.gitautocommit.llm.service
 
-import com.ibrahimharoon.gitautocommit.llm.model.LlmMessage
-
 interface LlmResponseService {
     /**
      * This function is used to create a baseline behavior for all LLM Response Services
      *
      * @param role: The behavior/persona for the model to inherit
      * @param prompt: The task for the model to complete (usually contextualized through a RAG framework)
-     * @param conversationHistory: All previous conversations between user and assistant for history context
      * @return: response from LLM
      */
-    fun response(role: String, prompt: String, conversationHistory: List<LlmMessage>?): String
+    fun response(role: String, prompt: String): String
 }
