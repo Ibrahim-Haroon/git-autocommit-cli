f9fcaab758a78551494e742a785743d9c3cc2142
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2024-12-20 01:28:42 -0500

    added functionality to use conversation memory object for storing previous conversations. Made thread safe as well

--- src/main/kotlin/com/ibrahimharoon/gitautocommit/llm/service/GoogleVertexLlmResponseService.kt
+++ src/main/kotlin/com/ibrahimharoon/gitautocommit/llm/service/GoogleVertexLlmResponseService.kt
@@ -1,20 +1,38 @@
 package com.ibrahimharoon.gitautocommit.llm.service
 
+import com.ibrahimharoon.gitautocommit.llm.model.LlmMessage
 import com.ibrahimharoon.gitautocommit.rest.dtos.GoogleVertexResponseDto
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpMethod
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.client.exchange
+import java.util.concurrent.locks.ReentrantLock
+import kotlin.concurrent.withLock
 
 class GoogleVertexLlmResponseService(
     private val url: String,
     private val headers: HttpHeaders
 ) : LlmResponseService {
+    private val lock = ReentrantLock()
 
-    override fun response(role: String, prompt: String): String {
+    override fun response(role: String, prompt: String, conversationHistory: List<LlmMessage>?): String = lock.withLock {
+        val prevConversations = conversationHistory?.map { message ->
+            mapOf(
+                "role" to when (message.role.value) {
+                    "assistant" -> "model"
+                    else -> "user"
+                },
+                "parts" to listOf(
+                    mapOf(
+                        "text" to message.content
+                    )
+                )
+            )
+        } ?: emptyList()
+
         val payload = mapOf(
-            "contents" to listOf(
+            "contents" to prevConversations + listOf(
                 mapOf(
                     "role" to "user",
                     "parts" to listOf(
