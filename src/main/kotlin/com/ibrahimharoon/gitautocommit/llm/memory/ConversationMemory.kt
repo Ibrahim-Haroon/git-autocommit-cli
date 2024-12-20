0e1f0d1464c7ce4813e7d025f5c70b311e3127a9
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2024-12-20 00:43:34 -0500

    using this memory object to store all conversations between llm and user

--- src/main/kotlin/com/ibrahimharoon/gitautocommit/llm/memory/ConversationMemory.kt
+++ src/main/kotlin/com/ibrahimharoon/gitautocommit/llm/memory/ConversationMemory.kt
@@ -5,6 +5,9 @@ import com.ibrahimharoon.gitautocommit.llm.model.LlmMessage
 object ConversationMemory {
     private val store: MutableList<LlmMessage> = mutableListOf()
 
+    val history: List<LlmMessage>
+        get() = store.toList()
+
     fun add(message: LlmMessage) {
         store.add(message)
     }
