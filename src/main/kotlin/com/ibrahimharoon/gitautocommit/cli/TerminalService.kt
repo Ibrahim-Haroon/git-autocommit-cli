87ed27305eb8422239b0d0190ce9e65c4646ba8f
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2024-12-20 01:55:53 -0500

    feat(cli): Implement thread-safe locking in TerminalService
    - Introduce ReentrantLock to ensure thread safety in TerminalService methods
    - Use lock.withLock and EventQueue for copyToClipboard and executeCommand to prevent concurrent access
    - Modified executeCommand method to include timeout handling using TimeUnit and TimeoutException.
    - Added process destruction in the finally block.

--- src/main/kotlin/com/ibrahimharoon/gitautocommit/cli/TerminalService.kt
+++ src/main/kotlin/com/ibrahimharoon/gitautocommit/cli/TerminalService.kt
@@ -1,23 +1,31 @@
 package com.ibrahimharoon.gitautocommit.cli
 
+import java.awt.EventQueue
 import java.awt.Toolkit
 import java.awt.datatransfer.StringSelection
 import java.io.BufferedReader
 import java.io.InputStreamReader
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.TimeoutException
+import java.util.concurrent.locks.ReentrantLock
+import kotlin.concurrent.withLock
 
 /**
  * This object is used for interacting and executing commands in the Terminal
  */
 object TerminalService {
+    private val lock = ReentrantLock()
     /**
      * Copies the provided text to the system clipboard.
      *
      * @param text The text to be copied to the clipboard.
      */
-    fun copyToClipboard(text: String) {
-        val selection = StringSelection(text)
-        val clipboard = Toolkit.getDefaultToolkit().systemClipboard
-        clipboard.setContents(selection, null)
+    fun copyToClipboard(text: String) = lock.withLock {
+        EventQueue.invokeAndWait {
+            val selection = StringSelection(text)
+            val clipboard = Toolkit.getDefaultToolkit().systemClipboard
+            clipboard.setContents(selection, null)
+        }
     }
 
     /**
@@ -26,14 +34,19 @@ object TerminalService {
      * @param command A list of strings representing the command to be executed.
      * @return The output of the command as a string, or empty string if the command produced no output.
      */
-    fun executeCommand(command: List<String>): String {
+    fun executeCommand(command: List<String>): String = lock.withLock {
         val process = ProcessBuilder(command)
             .redirectErrorStream(true)
             .start()
 
-        val stdout = BufferedReader(InputStreamReader(process.inputStream)).readText()
-        process.waitFor()
-
-        return stdout.trim().takeIf { it.isNotEmpty() } ?: ""
+        try {
+            val stdout = BufferedReader(InputStreamReader(process.inputStream)).readText()
+            if (!process.waitFor(2, TimeUnit.SECONDS)) {
+                throw TimeoutException("Command execution timed out")
+            }
+            return stdout.trim().takeIf { it.isNotEmpty() } ?: ""
+        } finally {
+            process.destroy()
+        }
     }
 }
