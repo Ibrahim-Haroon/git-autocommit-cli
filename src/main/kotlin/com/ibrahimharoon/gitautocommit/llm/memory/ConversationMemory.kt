101086a1fd2f5e1d79599d2a802b855e78a351c1
Ibrahim Haroon <99413953+Ibrahim-Haroon@users.noreply.github.com>
2024-12-20 02:22:44 -0500

    Revert "using this memory object to store all conversations between llm and user"
    This reverts commit 0e1f0d1464c7ce4813e7d025f5c70b311e3127a9.

--- src/main/kotlin/com/ibrahimharoon/gitautocommit/llm/memory/ConversationMemory.kt
+++ src/main/kotlin/com/ibrahimharoon/gitautocommit/llm/memory/ConversationMemory.kt
@@ -5,9 +5,6 @@ import com.ibrahimharoon.gitautocommit.llm.model.LlmMessage
 object ConversationMemory {
     private val store: MutableList<LlmMessage> = mutableListOf()
 
-    val history: List<LlmMessage>
-        get() = store.toList()
-
     fun add(message: LlmMessage) {
         store.add(message)
     }
